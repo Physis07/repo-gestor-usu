eb18eb5ed581a4e44403e719e3466fdbb7d2c928
Jonathan Alher <jonyalmodovar@gmail.com>
2023-03-16 21:28:31 -0300

    Agregando el repo

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import useFormulario from "./hooks/useFormulario.js";
+import Input from "./components/Input.css";
+import Card from "./components/Card";
+import Container from "./components/Container";
+import Button from "./components/Button";
+
+function App() {
+  const [formulario, handleChange] = useFormulario({
+    name: "",
+    lastname: "",
+    email: " ",
+  });
+
+  return (
+    <Container>
+      <Card>
+        <div style={{ padding: 20 }}>
+          <form>
+            <Input
+              label="Nombre"
+              name="name"
+              value={formulario.name}
+              onChange={handleChange}
+            />
+            <Input
+              label="Apellido"
+              name="lastname"
+              value={formulario.lastname}
+              onChange={handleChange}
+            />
+            <Input
+              label="Correo"
+              name="email"
+              value={formulario.email}
+              onChange={handleChange}
+            />
+            <Button>Enviar</Button>
+          </form>
+        </div>
+      </Card>
+    </Container>
+  );
+}
+
+export default App;
